a712b3e02e16215c13b8f5a9b4fdf30a4e9e390b
Ryder <rdeback@gmail.com>
2021-07-12 14:56:47 -0700

    Working on understanding .net classes.

---
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ModelsAndModelBinding.Models;
+
+namespace ModelsAndModelBinding.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet] //When user navigates to URL.
+        public IActionResult Add()
+        {
+            return View();
+        }
+        //Gets called after user posts form data
+        [HttpPost]
+        public IActionResult Add(IFormCollection form)
+        {
+            Student stu = new Student();
+            stu.FullName = form["full_name"];
+            stu.DateOfBirth = Convert.ToDateTime(form["dob"]);
+            stu.EmailAddress = form["email"];
+            stu.PhoneNumber = form["home_phone"];
+
+            return View();
+        }
+    }
+}
